846a4aef7082551e0394e916effd883ed354da1c
muchachitta <muchachita@inbox.ru>
2021-03-23 23:45:50 +0300

    JS-модалка

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,28 @@
-const modalFeedback = document.querySelector('.modal-feedback');
+const linkFeedback = document.querySelector('.footer-button');
+const popupFeedback = document.querySelector('.modal-feedback');
+const btnFeedbackClose = document.querySelector('.button-close');
+const modalForm = document.querySelector('.modal-form');
+const inputName = document.querySelector('.feedback-input-name');
+const inputEmail = document.querySelector('.feedback-input-email');
+const inputMessage = document.querySelector('.feedback-input-message');
+
+
+
+linkFeedback.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popupFeedback.classList.add('modal-feedback-show');
+  inputName.focus();
+});
+
+/*linkFeedback.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  popupFeedback.classList.remove('modal-feedback-show');
+});*/
+
+modalForm.addEventListener('submit', function(evt) {
+  if(!inputName || !inputEmail || !inputMessage) {
+    evt.preventDefault();
+  }
+
+});
+
